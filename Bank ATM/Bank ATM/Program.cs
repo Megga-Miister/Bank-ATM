2d10c1442ab29b43ec011b95cd39d5b8aca0d54e
Meggan <meggtriplett@gmail.com>
2019-06-12 03:49:59 -0700

    Initial set up

---
+++ Bank ATM/Bank ATM/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Bank_ATM
+{
+    public class Program
+    {
+        public static double balance = 5000.00;
+
+        static void Main(string[] args)
+        {
+            //TODO - Greeting and call to userinterface
+
+            //TODO - General Exception Handler
+        }
+
+        static void UserInterface()
+        {
+
+        }
+
+        static void ViewBalance()
+        {
+
+        }
+
+        static void WithdrawMoney()
+        {
+
+        }
+
+        static void DepositMoney()
+        {
+
+        }
+
+        static void Exit()
+        {
+
+        }
+
+    }
+}
