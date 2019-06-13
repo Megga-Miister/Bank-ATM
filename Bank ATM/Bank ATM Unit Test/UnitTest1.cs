069cfb12753668dbd93e71dcce50c41775ca2642
Meggan <meggtriplett@gmail.com>
2019-06-12 18:58:11 -0700

    Project file scaffolding

---
+++ Bank ATM/Bank ATM Unit Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Bank_ATM;
+
+namespace Bank_ATM_Unit_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- Bank ATM/Bank ATM/Program.cs
+++ Bank ATM/Bank ATM/Program.cs
@@ -4,18 +4,46 @@ namespace Bank_ATM
 {
     public class Program
     {
-        public static double balance = 5000.00;
+        static double balance = 5000.00;
 
         static void Main(string[] args)
         {
-            //TODO - Greeting and call to userinterface
+            Console.WriteLine("Welcome to the Mocking Bird Bank ATM");
 
-            //TODO - General Exception Handler
+            try
+            {
+                UserInterface();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for using Mocking Bird Bank.");
+            }
         }
 
         static void UserInterface()
         {
+            Console.WriteLine("Please choose from one of the following options:");
+            Console.WriteLine("1) View Balance");
+            Console.WriteLine("2) Withdraw Money");
+            Console.WriteLine("3) Deposit Money");
+            Console.WriteLine("4) Exit");
 
+            string userSelection = Console.ReadLine();
+            int actionSelection = Convert.ToInt32(userSelection);
+
+            switch (actionSelection)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                default:
+                    break;
+            }
         }
 
         static void ViewBalance()
