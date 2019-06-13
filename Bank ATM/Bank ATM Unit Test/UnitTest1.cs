a8955ce891455027b1e7751a07be0779cb094c1a
Meggan <meggtriplett@gmail.com>
2019-06-12 23:47:00 -0700

    Added unit tests to confirm DepositMoney method is working properly.

--- Bank ATM/Bank ATM Unit Test/UnitTest1.cs
+++ Bank ATM/Bank ATM Unit Test/UnitTest1.cs
@@ -25,5 +25,25 @@ namespace Bank_ATM_Unit_Test
 
             Assert.Equal("5000", testAmount);
         }
+
+        [Fact]
+        public void CanDepositMoneyToIncreaseBalance()
+        {
+            Program.balance = 5000.00;
+
+            string testAmount = Convert.ToString(Program.DepositMoney(1800.00));
+
+            Assert.Equal("6800", testAmount);
+        }
+
+        [Fact]
+        public void CanNotDepositMoneyWithNegativeAmount()
+        {
+            Program.balance = 5000.00;
+
+            string testAmount = Convert.ToString(Program.DepositMoney(-400.00));
+
+            Assert.Equal("5000", testAmount);
+        }
     }
 }
